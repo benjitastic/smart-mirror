d1751308634cd8a2da4dfafdabff44d9604f2011
Ben <benjitastic@gmail.com>
2017-03-20 18:40:33 -0700

    updates

--- plugins/xkcd/controller.js
+++ plugins/xkcd/controller.js
@@ -1,14 +1,34 @@
-function Xkcd($scope, $http, SpeechService, Focus) {
+// function Xkcd($scope, $http, SpeechService, Focus) {
 
-    // Show xkcd comic
-    SpeechService.addCommand('image_comic', function () {
+//     // Show xkcd comic
+//     SpeechService.addCommand('image_comic', function () {
+//         $http.jsonp("http://dynamic.xkcd.com/api-0/jsonp/comic?callback=JSON_CALLBACK")
+//             .then(function (response) {
+//                 $scope.xkcd = response.data.img;
+//                 Focus.change("xkcd");
+//             });
+//     });
+
+// }
+
+// angular.module('SmartMirror')
+//     .controller('Xkcd', Xkcd);
+
+
+
+
+function Xkcd($scope, $http, $interval) {
+
+    getxkcd()
+    $interval(getxkcd, (1440 * 60000));
+
+    function getxkcd() {
         $http.jsonp("http://dynamic.xkcd.com/api-0/jsonp/comic?callback=JSON_CALLBACK")
             .then(function (response) {
                 $scope.xkcd = response.data.img;
-                Focus.change("xkcd");
+                // Focus.change("xkcd");
             });
-    });
-
+    }
 }
 
 angular.module('SmartMirror')
